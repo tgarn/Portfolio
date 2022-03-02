0d473670d919fc04408048e67cb68aa198b46b78
tommy garnett <Garnett1995@hotmail.com>
2022-03-02 08:48:36 -0800

    Add project files.

---
+++ Portfolio/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(() => {
+    $('.expand-down').on('click', event => {
+        $(event.currentTarget).children().slideToggle(300);
+    });
+    $('.skills-dropdown').on('click', event => {
+        $(event.currentTarget).children().toggle(300);
+    });
+    $('.enlarging-links').on('mouseenter', event => {
+        $(event.currentTarget).css('font-size','24px');
+    });
+    $('.enlarging-links').on('mouseleave', event => {
+        $(event.currentTarget).css('font-size', '16px');
+    });
+});
